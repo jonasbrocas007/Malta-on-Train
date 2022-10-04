0083471af61bd9e64fbb9ec013c1cbf7f635c2b6
jonasbrocas <55618670+jonasbrocas007@users.noreply.github.com>
2022-10-04 19:44:23 +0100

    Add files via upload

--- train.py
+++ train.py
@@ -3,8 +3,6 @@ if not __name__ == "__main__":
 import sys
 import os
 version = 'alpha 0.2'
-fp = open('config.txt','r')
-readline = fp.readlines()
 #print(readline[0])
 
 if sys.argv[1] == 'createmalta':
@@ -24,25 +22,9 @@ if sys.argv[0] == 'createmalta':
     info_open = open(info,'w')
     info_open.write('use maltalang')
 
-    #f.close()
-'''if sys.argv[1] == 'createpobre':
-    f = open('main.pbr','w')
-    info = str('config')
-    info = info.replace('.pbr','')
-    info = info+'.txt'
-    info_open = open(info,'w')
-    info_open.write('use pobrelang')
-    #f.close()
 
-if sys.argv[0] == 'createpobre':
-    f = open('main.pbr','w')
-    info = str('config')
-    info = info.replace('.pbr','')
-    info = info+'.txt'
-    info_open = open(info,'w')
-    info_open.write('use pobrelang')
-    #f.close()'''
-
+fp = open('config.txt','r')
+readline = fp.readlines()
 if readline[0] == 'use maltalang':
     if sys.argv[1] == 'version' or sys.argv[0] == 'version':
         print(version)
@@ -54,14 +36,3 @@ if readline[0] == 'use maltalang':
     if sys.argv[0] == 'run':
         f = open(sys.argv[0],'r')
         os.system('MaltaLang main.mlt')
-
-'''if readline[0] == 'use pobrelang':
-    if sys.argv[1] == 'version' or sys.argv[0] == 'version':
-        print(version)
-
-    if sys.argv[0] == 'version' or  sys.argv[0] == 'version':
-        print(version)
-
-    if sys.argv[1] == 'run':
-        os.system('python pobrelang.py main.pbr')
-    '''
