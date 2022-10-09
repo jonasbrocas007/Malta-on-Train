ef997d7a2d144538b6e8d98528fe43df98e8afdd
jonasbrocas <55618670+jonasbrocas007@users.noreply.github.com>
2022-10-09 19:28:36 +0100

    Adding version 1 and 2 suport

--- train.py
+++ train.py
@@ -5,14 +5,21 @@ import os
 version = 'alpha 0.2'
 #print(readline[0])
 
-if sys.argv[1] == 'createmalta':
+if sys.argv[1] == 'createmalta1':
     f = open('main.mlt','w')
     info = str('config')
     info = info.replace('.mlt','')
     info = info+'.txt'
     info_open = open(info,'w')
-    info_open.write('use maltalang')
-    #f.close()
+    info_open.write('use maltalang1')
+
+if sys.argv[1] == 'createmalta2':
+    f = open('main.mlt','w')
+    info = str('config')
+    info = info.replace('.mlt','')
+    info = info+'.txt'
+    info_open = open(info,'w')
+    info_open.write('use maltalang2')
 
 if sys.argv[0] == 'createmalta':
     f = open('main.mlt','w')
@@ -25,14 +32,28 @@ if sys.argv[0] == 'createmalta':
 
 fp = open('config.txt','r')
 readline = fp.readlines()
-if readline[0] == 'use maltalang':
+if readline[0] == 'use maltalang1':
     if sys.argv[1] == 'version' or sys.argv[0] == 'version':
         print(version)
 
     if sys.argv[0] == 'version' or  sys.argv[0] == 'version':
         print(version)
+
     if sys.argv[1] == 'run':
-        os.system('python interpreter.py main.mlt')
+        os.system('python malta1.py main.mlt')
+    if sys.argv[0] == 'run':
+        f = open(sys.argv[0],'r')
+        os.system('MaltaLang main.mlt')
+
+if readline[0] == 'use maltalang2':
+    if sys.argv[1] == 'version' or sys.argv[0] == 'version':
+        print(version)
+
+    if sys.argv[0] == 'version' or  sys.argv[0] == 'version':
+        print(version)
+
+    if sys.argv[1] == 'run':
+        os.system('python malta2.py main.mlt')
     if sys.argv[0] == 'run':
         f = open(sys.argv[0],'r')
         os.system('MaltaLang main.mlt')
